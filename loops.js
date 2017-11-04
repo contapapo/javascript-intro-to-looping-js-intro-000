411e99d605351f5f861f0caa02367412f8c1c5d8
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-04 14:36:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,16 +1,13 @@
-function forLoop(array){
-  array=[]
-  for (let i = 0; i < 25; i++) {
-
-    if (i === 1) {
-      array.push(`I am ${i} strange loop.`);
-    }
-    else {
-      array.push(`I am ${i} strange loops.`);
-    }}
-return array;
-
-}
+function forLoop(array) {
+ +   for ( let i = 0; i < 25; i++) {
+ +     if (i === 1) {
+ +       array.push(`I am 1 strange loop.`)
+ +     } else {
+ +       array.push(`I am ${i} strange loops.`)
+ +     }
+ +  }
+ +  return array;
+ +}
 
 function whileLoop(n){
   while (n > 0){
@@ -19,8 +16,6 @@ function whileLoop(n){
   return "done";
 }
 
-
-
 function doWhileLoop(array){
   function maybeTrue() {
     return Math.random() >= 0.5
@@ -28,6 +23,5 @@ function doWhileLoop(array){
   do {
     array.pop;
   } while (array.length === 0  && maybeTrue());
-
   return array;
 }
