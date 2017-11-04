bd498628b63f9555f24753551faf1102652fdf9b
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-04 13:11:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,14 @@
-function forLoop(){
+function forLoop(array){
   for (var i = 0; i <= 25; i++) {
-    var resp=[];
+    
     if (i <== 1) {
-      resp.push (`I am ${i} strange loop.`);
+      array.push (`I am ${i} strange loop.`);
 
     }
     else {
-      resp.push (`I am ${i} strange loops.`);
+      array.push (`I am ${i} strange loops.`);
 
     }
-return resp;
+return array;
 }
 }
